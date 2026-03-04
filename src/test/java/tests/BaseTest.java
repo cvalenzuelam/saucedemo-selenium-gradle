b815b7c2e9f2dbaab3f980c16a81712984504260
cvalenzuelam <cvalenzuelam92@gmail.com>
2026-03-04 14:23:29 -0700

    fix: increase timeout and add more Chrome options for stability

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -18,6 +18,8 @@ public class BaseTest {
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
         
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
