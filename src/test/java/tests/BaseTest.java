011ae91565e677dcbc5203dfd74e870a3865bbf9
cvalenzuelam <cvalenzuelam92@gmail.com>
2026-03-04 18:05:34 -0700

    fix: add Chrome CI options and update workflow

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -22,10 +22,11 @@ public class BaseTest {
         Logger.getLogger("org.openqa.selenium").setLevel(Level.OFF);
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); 
+        options.addArguments("--headless=new"); 
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
+        options.addArguments("--disable-setuid-sandbox");
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--remote-allow-origins=*");
         
